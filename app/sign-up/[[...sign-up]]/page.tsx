57152281617899be4ce8b7d75aaeb114a1302c65
Scott Martin <scott.martin88@live.com>
2024-10-08 07:19:44 +0200

    Modify layout of sign up page

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,15 @@
-import { SignUp } from '@clerk/nextjs'
+import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />
+  return (
+    <div className="flex-1 grid gap-5 lg:grid-cols-2 justify-center items-center p-5 overflow-scroll bg-floral-500">
+      <div className="text-center">
+        <h1 className="text-5xl font-semibold mb-3">
+          Recipe<span className="text-bluegreen-500">Gen</span>
+        </h1>
+        <p>This is a description about the app</p>
+      </div>
+      <SignUp />
+    </div>
+  );
 }
